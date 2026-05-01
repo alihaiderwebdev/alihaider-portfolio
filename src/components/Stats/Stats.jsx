8dd0afe173d3446b348569e193237ccfc26771c4
ali haider <alihaidersinfo@gmail.com>
2026-05-01 05:41:44 +0500

    stats component content added

---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,17 @@
+import { stats } from "../data/portfolio";
+import "./Stats.css";
+
+export default function Stats() {
+  return (
+    <div className="stats-banner">
+      <div className="container stats-banner__inner">
+        {stats.map((stat, i) => (
+          <div key={i} className="stats-banner__item">
+            <span className="stats-banner__val">{stat.value}</span>
+            <span className="stats-banner__lbl">{stat.label}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
